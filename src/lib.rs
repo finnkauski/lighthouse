d0629da09682ae93cbe083dfa12c9bb629080dce
finnkauski <iwiivi@gmail.com>
2020-06-28 17:33:34 +0100

    add: core sending functionality. Need to add: all

--- src/lib.rs
+++ src/lib.rs
@@ -34,34 +34,48 @@ pub mod helpers {
         }
     }
 
-    type RequestBatch = Vec<(Url, AllowedMethod)>;
-    type NewStates = Vec<Option<SendableState>>;
+    /// This type alias is a URL and a type of Request to be sent
+    pub type RequestTarget = (Url, AllowedMethod);
+
+    /// This alias allows us to store multiple targeted lights
+    type RequestTargets = Vec<RequestTarget>;
+
+    /// NewStates is a convenience alias allowing us to store
+    /// optional vectors of states that we want to send if we
+    /// are using PUT or POST to a given endpoint
+    type NewStates<'a> = Vec<Option<&'a SendableState>>;
+
+    /// Convenience type alias for a possible Result from the reqwest client
     type ResponseResult = Result<reqwest::Response, reqwest::Error>;
 
     /// Function wrapping the request sending functionality
     /// to a location.
-    pub async fn future_request(
-        target: Url,
-        method: AllowedMethod,
-        params: Option<SendableState>,
+    pub async fn send_request(
+        request_target: RequestTarget,
+        state: Option<&SendableState>,
     ) -> ResponseResult {
         let client = reqwest::Client::new();
+        let (target, method) = request_target;
         match method {
-            AllowedMethod::POST => client.post(target).json(&params).send().await,
+            AllowedMethod::POST => client.post(target).json(&state).send().await,
             AllowedMethod::GET => client.get(target).send().await,
-            AllowedMethod::PUT => client.put(target).json(&params).send().await,
+            AllowedMethod::PUT => client.put(target).json(&state).send().await,
         }
     }
 
-    /// Function that can generate a asyncronous object
-    /// that will go off
-    #[tokio::main]
-    pub async fn requests(request_batch: RequestBatch, states: NewStates) -> Vec<ResponseResult> {
+    /// Function that sends off several states to the lights
+    /// This is much more key than individual requests functionality provided by the
+    /// send_request function as this is allowing us to do this asynchronously across
+    /// an arbitrary selection of lights.
+    pub async fn send_requests(
+        request_targets: RequestTargets,
+        states: NewStates<'_>,
+    ) -> Vec<ResponseResult> {
         futures::future::join_all(
-            request_batch
+            request_targets
                 .into_iter()
                 .zip(states.into_iter())
-                .map(|((url, method), state)| future_request(url, method, state)),
+                .map(|(target, state)| send_request(target, state)),
         )
         .await
     }
@@ -70,6 +84,8 @@ pub mod helpers {
 pub mod bridge {
     // imports
     use super::helpers::*;
+    use super::lights::*;
+    use std::collections::BTreeMap;
     use std::net::IpAddr;
     use url::Url;
 
@@ -78,13 +94,36 @@ pub mod bridge {
     #[derive(Debug)]
     pub struct Bridge {
         pub target: Url,
+        runtime: tokio::runtime::Runtime,
     }
 
     impl Bridge {
         /// Constructor for a bridge from and IP and a token
         pub fn new(ip: IpAddr, token: &str) -> Result<Self, BridgeError> {
             let target = generate_target(ip, token)?;
-            Ok(Bridge { target })
+            let runtime = tokio::runtime::Runtime::new().unwrap(); // TODO: this is not safe
+            Ok(Bridge { target, runtime })
+        }
+
+        pub fn scan(&mut self) -> BTreeMap<u8, Light> {
+            let endpoint = self.get_endpoint("./lights", AllowedMethod::GET);
+            let lights: BTreeMap<u8, Light> = self
+                .runtime
+                .block_on(async { send_request(endpoint, None).await?.json().await })
+                .expect("Could not completely decode/send request");
+            lights
+        }
+
+        pub fn state_to(&mut self, id: u8, new_state: &SendableState) -> reqwest::Response {
+            let endpoint =
+                self.get_endpoint(&format!("./lights/{}/state", id)[..], AllowedMethod::PUT);
+            self.runtime
+                .block_on(send_request(endpoint, Some(new_state)))
+                .expect(&format!("Could not send state to light: {}", id)[..])
+        }
+
+        fn get_endpoint(&self, s: &str, method: AllowedMethod) -> RequestTarget {
+            (self.target.join(s).unwrap(), method)
         }
 
         /// Method to find bridge ip addressed on the network
@@ -120,6 +159,7 @@ pub mod bridge {
 pub mod lights {
     // imports
     use serde::{Deserialize, Serialize};
+    use serde_json::Value;
 
     /// Struct that can be sent to the Hue lights. It mirrors closely the
     /// `State`.
@@ -172,6 +212,56 @@ pub mod lights {
         }
     }
 
+    /// This object contains the state part  of each light
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    pub struct State {
+        pub on: bool,
+        pub bri: u8,
+        pub hue: u16,
+        pub sat: u8,
+        pub effect: String,
+        pub xy: [f32; 2],
+        pub ct: u32,
+        pub alert: String,
+        pub colormode: String,
+        pub mode: String,
+        pub reachable: bool,
+    }
+
+    impl From<State> for SendableState {
+        fn from(state: State) -> Self {
+            Self {
+                on: Some(state.on),
+                bri: Some(state.bri),
+                hue: Some(state.hue),
+                sat: Some(state.sat),
+                effect: Some(state.effect),
+                xy: Some(state.xy),
+                alert: None,
+                transitiontime: Some(1),
+                colormode: Some(state.colormode),
+            }
+        }
+    }
+
+    /// Light object representing the complete state of a light
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Light {
+        pub state: State,
+        pub swupdate: Value,
+        pub r#type: String,
+        pub name: String,
+        pub modelid: String,
+        pub manufacturername: String,
+        pub productname: String,
+        pub capabilities: Value,
+        pub config: Value,
+        pub uniqueid: String,
+        pub swversion: String,
+        pub swconfigid: String,
+        pub productid: String,
+    }
+
     /// Super useful macro to create `SendableState`
     /// ```
     /// use lighthouse::*;
@@ -184,19 +274,26 @@ pub mod lights {
     /// ```
     #[macro_export]
     macro_rules! state {
-    (ref, $($i:ident:$v:expr), *) => {
-        &SendableState {
+    ($($i:ident:$v:expr), *) => {
+        &lighthouse::lights::SendableState {
             $($i: Some($v),) *
-            ..SendableState::default()
+            ..lighthouse::lights::SendableState::default()
         }
     };
 
-    ($($i:ident:$v:expr), *) => {
-        SendableState {
-            $($i: Some($v),) *
-            ..SendableState::default()
+    (non-ref; $($i:ident:$v:expr),*) => {
+        lighthouse::lights::SendableState {
+            $($i: Some($v),)*
+            ..lighthouse::lights::SendableState::default()
         }
     };
+
+    (from: $state:expr; $($i:ident:$v:expr),*) => {{
+        let mut sendable = lighthouse::lights::SendableState::from($state);
+        $(sendable.$i = Some($v);)*
+        sendable
+    }};
+
 
 }
 }
